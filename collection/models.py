87ef16e3d1bfd9ce7d3a58778be8e71fe6e6d343
salemandreus <salemandreus@gmail.com>
2018-05-26 11:54:23 +0200

    added day name to weather page model

--- collection/models.py
+++ collection/models.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
-
-# Create your models here.
+class Day(models.Model):
+    date = models.CharField(max_length=255)
+    day = models.CharField(max_length=255)
+    hightemp = models.CharField(max_length=255) 
+    lowtemp = models.CharField(max_length=255)
+    wind = models.CharField(max_length=255)
+    rain = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
