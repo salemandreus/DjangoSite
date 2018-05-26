02fafffcc5b2a4a0887ed10b90183667dc74b267
salemandreus <salemandreus@gmail.com>
2018-05-26 12:35:20 +0200

    fixed wrong name in admin model, added test data to db

--- collection/admin.py
+++ collection/admin.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from collection.models import Day
 
-# Register your models here.
+class DayAdmin(admin.ModelAdmin):
+    model = Day
+    list_display = ('date', 'day', 'hightemp', 'lowtemp', 'wind', 'rain')
+    prepopulated_fields = {'slug': ('day',)}
+
+admin.site.register(Day, DayAdmin)
