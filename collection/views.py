ca935704bded1300097d8972052db1271c33d450
salemandreus <salemandreus@gmail.com>
2018-05-26 13:19:50 +0200

    fixed variable names so website main page properly displays weather objects

--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,11 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from collection.models import Day
 
-def index(request): 
-    return render(request, 'index.html')
+def index(request):
+    days = Day.objects.all()
+
+    return render(request, 'index.html', {
+        'days': days,
+    })
