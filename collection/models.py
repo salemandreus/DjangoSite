ce8ce92464b89a9146d6af0e1f8549ac14b60b70
salemandreus <salemandreus@gmail.com>
2018-05-26 12:07:50 +0200

    fixed missing import on Day model, created model

--- collection/models.py
+++ collection/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from django.db import models
+
 class Day(models.Model):
     date = models.CharField(max_length=255)
     day = models.CharField(max_length=255)
