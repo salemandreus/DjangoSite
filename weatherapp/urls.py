755cbf02791d8468f84ed2cc199cccd7b2c4e54c
salemandreus <salemandreus@gmail.com>
2018-05-27 23:45:05 +0200

    added login and register functionality. Currently this is for data adding purposes not viewing purposes.

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from collection import views
@@ -6,5 +6,6 @@ from collection import views
 urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^days/(?P<slug>[-\w]+)/$', views.day_detail, name='day_detail'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
